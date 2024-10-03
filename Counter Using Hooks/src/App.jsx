166882e8e59fcca590a41d268813d27eb5860600
Divyanshu <123636880+Divyanshuk09@users.noreply.github.com>
2024-10-03 21:11:33 +0000

    Counter

---
+++ Counter Using Hooks/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+/*
+function App() {
+  let counter = 5;
+  const addvalue = () => {            //Value change hui pr ui pr nhi aagyi thats why we use hooks
+    console.log(counter)
+    counter = counter + 1
+  }
+*/
+
+function App() {
+  let [counter, setCounter] = useState(5)
+
+  const addvalue = () => {
+    if (counter < 20) {
+      counter = counter + 1
+      setCounter(counter)
+    }
+  }
+  const subtractvalue = () => {
+    if (counter > 0) {
+      counter = counter - 1
+      setCounter(counter)
+    }
+  }
+  return (
+    <>
+      <h1>Counter Using Hooks</h1>
+      <h2>Counter Value : {counter}</h2>
+      <div className='buttons'>
+        <button className='addbutton' onClick={addvalue} disabled={counter===20}>Add Value</button>
+        <button className='subbutton' onClick={subtractvalue} disabled={counter===0}>Subtract Value</button>
+      </div>
+    </>
+  )
+}
+
+export default App
