a4a00d6f184e55a50144069ceceb4fe66787dd5f
Divyanshu <123636880+Divyanshuk09@users.noreply.github.com>
2024-10-05 07:56:42 +0000

    Counter

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -4,10 +4,11 @@ import './App.css'
 function App() {
   let counter = 5;
   const addvalue = () => {            //Value change hui pr ui pr nhi aagyi thats why we use hooks
-    console.log(counter)
-    counter = counter + 1
-  }
-*/
+  console.log(counter)              /*Hooks se React development ka process aur bhi efficient aur manageable ho gaya hai.       
+  counter = counter + 1               Yeh naye developers ke liye learning curve ko bhi asaan karte hain, kyunki unhe       
+  }                                   sirf functional components ke concepts ko samajhna hota hai */                            
+    
+
 
 function App() {
   let [counter, setCounter] = useState(5)
