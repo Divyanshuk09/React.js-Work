a6d9db21c9262140f6f2676e4af5dde16b521f58
Divyanshu <123636880+Divyanshuk09@users.noreply.github.com>
2024-10-05 07:59:36 +0000

    UseState

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -10,15 +10,24 @@ function App() {
   return (
     <div className='w-screen h-screen' style={{ backgroundColor: color }}>
       <div className="fixed bottom-32 left-[20%] m-auto bg-white px-3 py-[6px] flex flex-wrap gap-4 justify-center rounded-3xl">
-        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'red' }} onClick={() => setColor("red")} >Red</button>
-        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'white' }} onClick={() => setColor("white")}>White</button>
-        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'pink' }} onClick={() => setColor("pink")}>Pink</button>
-        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'yellow' }} onClick={() => setColor("yellow")}>Yellow</button>
-        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'orange' }} onClick={() => setColor("orange")}>Orange</button>
-        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'green' }} onClick={() => setColor("green")}>Green</button>
-        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'olive' }} onClick={() => setColor("olive")}>Olive</button>
-        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'grey' }} onClick={() => setColor("grey")}>Grey</button>
-        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'violet' }} onClick={() => setColor("violet")}>Violet</button>
+        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'red' }} onClick={() =>       
+           setColor("red")} >Red</button>
+        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'white' }} onClick={() => 
+           setColor("white")}>White</button>
+        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'pink' }} onClick={() => 
+           setColor("pink")}>Pink</button>
+        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'yellow' }} onClick={() => 
+           setColor("yellow")}>Yellow</button>
+        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'orange' }} onClick={() => 
+           setColor("orange")}>Orange</button>
+        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'green' }} onClick={() => 
+           setColor("green")}>Green</button>
+        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'olive' }} onClick={() => 
+           setColor("olive")}>Olive</button>
+        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'grey' }} onClick={() => 
+           setColor("grey")}>Grey</button>
+        <button className='px-6 py-1 rounded-2xl border border-black' style={{ backgroundColor: 'violet' }} onClick={() => 
+           setColor("violet")}>Violet</button>
       </div>
     </div>
   )
