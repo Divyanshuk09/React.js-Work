f5ea6e71c7bf4e576d596f1ebc7ae27db9e2dfe7
Divyanshu <123636880+Divyanshuk09@users.noreply.github.com>
2024-10-05 08:00:14 +0000

    UseState

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   const addvalue = () => {            //Value change hui pr ui pr nhi aagyi thats why we use hooks
   console.log(counter)              /*Hooks se React development ka process aur bhi efficient aur manageable ho gaya hai.       
   counter = counter + 1               Yeh naye developers ke liye learning curve ko bhi asaan karte hain, kyunki unhe       
-  }                                   sirf functional components ke concepts ko samajhna hota hai */                            
-    
+  }                                   sirf functional components ke concepts ko samajhna hota hai */
+
 
 
 function App() {
@@ -30,8 +30,8 @@ function App() {
       <h1>Counter Using Hooks</h1>
       <h2>Counter Value : {counter}</h2>
       <div className='buttons'>
-        <button className='addbutton' onClick={addvalue} disabled={counter===20}>Add Value</button>
-        <button className='subbutton' onClick={subtractvalue} disabled={counter===0}>Subtract Value</button>
+        <button className='addbutton' onClick={addvalue} disabled={counter === 20}>Add Value</button>
+        <button className='subbutton' onClick={subtractvalue} disabled={counter === 0}>Subtract Value</button>
       </div>
     </>
   )
