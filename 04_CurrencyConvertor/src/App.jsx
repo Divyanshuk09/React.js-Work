261470679cc1135981d46d0f4ffdcf12dbfd7976
Divyanshu <123636880+Divyanshuk09@users.noreply.github.com>
2024-10-06 08:49:00 +0000

    Self Hook

--- 04_CurrencyConvertor/src/App.jsx
+++ 04_CurrencyConvertor/src/App.jsx
@@ -4,10 +4,9 @@ import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 function App() {
   const [amount, setAmount] = useState(0);
-  const [from, setFrom] = useState("usd");
-  const [to, setTo] = useState("inr");
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("INR");
   const [convertedAmt, setConvertedAmt] = useState(0);
-
   const currencyData = useCurrencyInfo(from);
   const options = Object.keys(currencyData || {});
 
@@ -16,7 +15,6 @@ function App() {
     setTo(from);
     setConvertedAmt(amount);
     setAmount(convertedAmt);
-    
   };
 
   const convert = () => {
@@ -29,10 +27,12 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        backgroundImage: `url('https://images.pexels.com/photos/1114613/pexels-photo-1114613.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load')`,
+        backgroundImage: `url('https://static.vecteezy.com/system/resources/previews/010/502/852/non_2x/light-and-dark-background-bitcoin-crypto-currency-illustration-for-page-logo-card-banner-web-and-printing-free-vector.jpg')`,
       }}
     >
       <div className="w-full">
+      <p className='w-full max-w-md mx-auto  text-white text-center my-5 text-2xl'>Currency Convertor</p>
+        
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form
             onSubmit={(e) => {
@@ -53,7 +53,7 @@ function App() {
             <div className="relative w-full h-0.5">
               <button
                 type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 hover:bg-blue-800 "
                 onClick={swap}
               >
                 Swap
@@ -69,7 +69,7 @@ function App() {
                 amountDisable
               />
             </div>
-            <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+            <button type="submit" className="w-full bg-blue-600 hover:bg-blue-800 text-white px-4 py-3 rounded-lg">
               Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
           </form>
--- 04_CurrencyConvertor/src/components/InputBox.jsx
+++ 04_CurrencyConvertor/src/components/InputBox.jsx
@@ -16,7 +16,7 @@ function InputBox({
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className='w-1/2'>
-        <label className='text-black/40 mb-2 inline-block' htmlFor={amountInputID} >{label}
+        <label className='text-gray-600 mb-2 inline-block' htmlFor={amountInputID} >{label}
         </label>
         <input className='outline-none w-full bg-transparent py-1.5'
           id='amountInputID'
@@ -28,11 +28,11 @@ function InputBox({
         />
       </div>
       <div className='w-1/2 flex flex-wrap justify-end text-right'>
-        <p className="text-black/40 mb-2 w-full"> Currency Type
+        <p className="text-gray-600 mb-2 w-full"> Currency Type
         </p>
         <select
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-          value={selectCurrency} // Ensures that the selected value is controlled
+          value={selectCurrency} 
           onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
           disabled={currencyDisable}
         >
